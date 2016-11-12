b9fe2663d2a21b79b7ace7eae111c9be0767c722
Jun Trinh <dungtrinh1993@gmail.com>
2016-11-12 11:20:48 -0800

    file json

--- watson/DataNewsBeta.py
+++ watson/DataNewsBeta.py
@@ -1,46 +1,60 @@
 import json
 from watson_developer_cloud import AlchemyDataNewsV1
 import sys
-import 
+import xml.etree.ElementTree as xmlParser
+from bs4 import  BeautifulSoup as bs
 
 
 def collectNews(company, timeBegin, timeEnd):
     alchemy_data_news = AlchemyDataNewsV1(api_key='d4a97f67bd08aa3720bd3be9dc5c92ad720f16fa')
 
     #results = alchemy_data_news.get_news_documents(start='now-60d', end='now', time_slice='12h')
-
     # print(json.dumps(results, indent=2))
-    stock_code = 'AAPL'
+    company_query = '|text=' + company + ',type=company|'
     results = alchemy_data_news.get_news_documents(
     start=timeBegin,
     end=timeEnd,
+
     #start='1453334400',
     #end='1454022000',
     #time_slice='12h',
     return_fields=['enriched.url.title',
-                   'enriched.url.url',
+                   #'enriched.url.url',
                    #'enriched.url.author',
                    #'enriched.url.publicationDate',
                    'enriched.url.entities.entity.sentiment.type',
                    #'enriched.url.keywords.keyword.sentiment.mixed'
                    'enriched.url.entities.entity.sentiment.score'
                    ],
-    query_fields={'q.enriched.url.enrichedTitle.entities.entity': '|text=IBM,type=company|'})
+    query_fields={'q.enriched.url.enrichedTitle.entities.entity': company_query})
     r = json.dumps(results, indent=2)
     return r
 
 
-def parseNew(file):
-    pass
+def parseNew(data):
+    """
+    :param file: json file
+    :return: sentiment analysis
+    """
+    score = 0
+    #data = json.loads(file)
+
+    #parse = bs(data["score"])
+    for each score in bs(data["score"]):
+    #print parse
+    #print parse.result.docs.find_all("source")
+    #print data.get("source",{})
+
+    return score
 
 
 def main(argv):
-    company = "IBM"
+    company = "Apple"
     json_file = collectNews(company, 'now-60d', 'now')
-    f = open('DataNewsBeta', 'w')
+    f = open('DataNewsBeta.json', 'w')
     f.write(str(json_file))
     print ("Complete Data Collecting")
-    parseNew(json_file)
+    print parseNew(json_file)
 
 if __name__ == "__main__":
     main(sys.argv)
