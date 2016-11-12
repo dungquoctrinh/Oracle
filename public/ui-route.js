82e869ceaf6a77326cf25761ecf8b4c8da26704a
Yiupang <leopangchan@gmail.com>
2016-11-12 00:00:40 -0800

    Finished Angular file system

---
+++ public/ui-route.js
@@ -0,0 +1,21 @@
+app.config(['$stateProvider', '$urlRouterProvider',
+   function($stateProvider, $router) {
+      //redirect to home if path is not matched
+      $router.otherwise("/");
+      $stateProvider
+      .state('home',  {
+         url: '/',
+         // TODO: templateUrl: 'Home/home.template.html',
+         controller: 'homeController',
+      })
+      .state('chatPage', {
+         url: '/ChatPage',
+         // TODO: templateUrl: 'ChatPage/',
+         controller: 'chatPageController',
+      })
+      .state('stockDetail', {
+         url: '/stockDetail',
+         // TODO: templateUrl: 'Customer/customer.template.html',
+         controller: 'stockDetailController',
+      })
+   }]);
