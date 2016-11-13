ea00499560810e36100b2586c5efabd5aa4ea01b
Jun Trinh <dungtrinh1993@gmail.com>
2016-11-12 16:15:12 -0800

    commit the new code

--- watson/DataNewsBeta.py
+++ watson/DataNewsBeta.py
@@ -20,14 +20,11 @@ def collectNews(company, timeBegin, timeEnd):
     timeEnd = 'now'
     #results = alchemy_data_news.get_news_documents(start='now-60d', end='now', time_slice='12h')
     # print(json.dumps(results, indent=2))
+
     company_query = '|text=' + company + ',type=company|'
     results = alchemy_data_news.get_news_documents(
     start=timeBegin,
     end=timeEnd,
-
-    #start='1453334400',
-    #end='1454022000',
-    #time_slice='12h',
     return_fields=['enriched.url.title',
                    #'enriched.url.url',
                    #'enriched.url.author',
@@ -41,7 +38,7 @@ def collectNews(company, timeBegin, timeEnd):
     return r
 
 
-def parseNew(name):
+def parseNews(name):
     """
     :param file: json file
     :return: sentiment analysis
@@ -75,21 +72,48 @@ def parseNew(name):
     return score/count
 
 
+def predictionSentiment(company):
+    """
+    Get the news from the most recent 2 days
+    :param company:
+    :return:
+    """
+    #change the key for the API in here. This is the AlchemyDataNews
+    KEY = '554259945902490161462f7e78bbf5829106ee4f'
+    alchemy_data_news = AlchemyDataNewsV1(api_key=KEY)
+    timeBegin ='now-2d'
+    timeEnd = 'now'
+    company_query = '|text=' + company + ',type=company|'
+    results = alchemy_data_news.get_news_documents(
+    start=timeBegin,
+    end=timeEnd,
+    return_fields=['enriched.url.title',
+                   'enriched.url.entities.entity.sentiment.type',
+                   'enriched.url.entities.entity.sentiment.score'
+                   ],
+    query_fields={'q.enriched.url.enrichedTitle.entities.entity': company_query})
+    r = json.dumps(results, indent=2)
+    f = open("/home/kid/Github/Oracle/watson/jsonp2.json", 'w')
+    f.write(str(r))
+
 def main(argv):
 
-    #company = "Apple"
+    company = "Apple"
     #format dd/mm/Y
+
     #API call has problem
-    #json_file = collectNews(company, "1/11/2016", "9/11/2016")
-    #f = open("/home/kid/Github/Oracle/watson/jsonp.json", 'w')
-    #f.write(str(json_file))
+    json_file = collectNews(company, "1/11/2016", "9/11/2016")
+    f = open("/home/kid/Github/Oracle/watson/jsonp.json", 'w')
+    f.write(str(json_file))
 
-
+    """
     #with open("/home/kid/Github/Oracle/watson/jsonp.json", 'w') as outfile:
     #    json.dump(json_file, outfile)
+    """
 
+    predictionSentiment(company)
     print ("Complete Data Collecting")
-    print parseNew("IBM")
+    #print parseNews("IBM")
 
 if __name__ == "__main__":
     main(sys.argv)
